38e70b50081e4b1325d4474724386e44e5f860bf
Dwayne Slater <ds84182@gmail.com>
2016-07-31 20:12:26 -0400

    Fix SetUniformCommand stupidly waiting until it's updated to apply changes

--- src/command/set_uniform.hpp
+++ src/command/set_uniform.hpp
@@ -28,47 +28,45 @@ public:
   using RenderCommand::RenderCommand;
 
   virtual void doRender(RenderThread *thread, bool justUpdated) {
-    if (justUpdated) {
-      GLint location = thread->shader->getUniformLocation(data.name);
-      auto count = data.count;
-      SneekyPointer dataptr = data.data.data();
-      switch (data.type) {
-        case UniformType::Float:
-          glUniform1fv(location, count, dataptr);
-          break;
-        case UniformType::Float2:
-          glUniform2fv(location, count, dataptr);
-          break;
-        case UniformType::Float3:
-          glUniform3fv(location, count, dataptr);
-          break;
-        case UniformType::Float4:
-          glUniform4fv(location, count, dataptr);
-          break;
+    GLint location = thread->shader->getUniformLocation(data.name);
+    auto count = data.count;
+    SneekyPointer dataptr = data.data.data();
+    switch (data.type) {
+      case UniformType::Float:
+        glUniform1fv(location, count, dataptr);
+        break;
+      case UniformType::Float2:
+        glUniform2fv(location, count, dataptr);
+        break;
+      case UniformType::Float3:
+        glUniform3fv(location, count, dataptr);
+        break;
+      case UniformType::Float4:
+        glUniform4fv(location, count, dataptr);
+        break;
 
-        case UniformType::Int:
-          glUniform1iv(location, count, dataptr);
-          break;
-        case UniformType::Int2:
-          glUniform2iv(location, count, dataptr);
-          break;
-        case UniformType::Int3:
-          glUniform3iv(location, count, dataptr);
-          break;
-        case UniformType::Int4:
-          glUniform4iv(location, count, dataptr);
-          break;
+      case UniformType::Int:
+        glUniform1iv(location, count, dataptr);
+        break;
+      case UniformType::Int2:
+        glUniform2iv(location, count, dataptr);
+        break;
+      case UniformType::Int3:
+        glUniform3iv(location, count, dataptr);
+        break;
+      case UniformType::Int4:
+        glUniform4iv(location, count, dataptr);
+        break;
 
-        case UniformType::Matrix2:
-          glUniformMatrix2fv(location, count, false, dataptr);
-          break;
-        case UniformType::Matrix3:
-          glUniformMatrix3fv(location, count, false, dataptr);
-          break;
-        case UniformType::Matrix4:
-          glUniformMatrix4fv(location, count, false, dataptr);
-          break;
-      }
+      case UniformType::Matrix2:
+        glUniformMatrix2fv(location, count, false, dataptr);
+        break;
+      case UniformType::Matrix3:
+        glUniformMatrix3fv(location, count, false, dataptr);
+        break;
+      case UniformType::Matrix4:
+        glUniformMatrix4fv(location, count, false, dataptr);
+        break;
     }
   }
 };
